b24a4b3d293db138dec89dde5a134ed2752ba2e5
Aleksandr Davydochkin <f1reb411111@gmail.com>
2022-01-30 23:15:02 +0300

    Show pokemons on mainpage

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -4,6 +4,7 @@ import json
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
+from .models import Pokemon, PokemonEntity
 
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = (
@@ -39,12 +40,18 @@ def show_all_pokemons(request):
                 pokemon['img_url']
             )
 
+    pokemons = Pokemon.objects.all()
+
     pokemons_on_page = []
     for pokemon in pokemons:
+        try:
+            image_url = request.build_absolute_uri(pokemon.image.url)
+        except ValueError:
+            image_url = None
         pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
+            'pokemon_id': pokemon.id,
+            'img_url': image_url,
+            'title_ru': pokemon.title,
         })
 
     return render(request, 'mainpage.html', context={
